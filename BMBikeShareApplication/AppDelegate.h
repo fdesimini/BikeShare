b8ba5330f4d8f09d49b4ca9393228fd877bd8c7e
kunwardeep <kunwardeep.gill@gmail.com>
2015-05-04 11:46:49 -0400

    Initial Commit

---
+++ BMBikeShareApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMBikeShareApplication
+//
+//  Created by Kunwardeep Gill on 2015-05-04.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMBikeShareApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMBikeShareApplication
+//
+//  Created by Kunwardeep Gill on 2015-05-04.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
